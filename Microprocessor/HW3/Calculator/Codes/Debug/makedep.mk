8ab76632b9632bcad86c1dafcaf8d1568af3f4fa
Maryam Saeedmehr <maryamsaeedmehr@ec.iut.ac.ir>
2020-01-31 19:29:07 +0330

    Add files via upload

---
+++ Microprocessor/HW3/Calculator/Codes/Debug/makedep.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Calculator.c
+
+calculator_blocks.c
+
+SourceCodes\keypad.c
+
+SourceCodes\lcd.c
+
