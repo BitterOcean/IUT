8f896072a8893f60fde5b2b71589189fde0be595
Maryam Saeedmehr <60509979+BitterOcean@users.noreply.github.com>
2020-01-31 17:38:54 +0330

    Add files via upload

---
+++ Algorithm/HW6/AdSMS.py
@@ -0,0 +1,53 @@
+from collections import defaultdict
+
+# Class to represent a graph
+class Graph:
+    def __init__(self, vertices):
+        self.graph = defaultdict(list)  # dictionary containing adjacency List
+        self.V = vertices  # No. of vertices
+
+    # function to add an edge to graph
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+    # A recursive function used by topologicalSort
+    def topologicalSortUtil(self, v, visited, stack):
+
+        # Mark the current node as visited.
+        visited[v] = True
+
+        # Recur for all the vertices adjacent to this vertex
+        for i in self.graph[v]:
+            if visited[i] == False:
+                self.topologicalSortUtil(i, visited, stack)
+
+        # Push current vertex to stack which stores result
+        stack.insert(0, v)
+
+    # The function to do Topological Sort. It uses recursive
+    # topologicalSortUtil()
+    def topologicalSort(self):
+        # Mark all the vertices as not visited
+        visited = [False]*self.V
+        stack = []
+
+        # Call the recursive helper function to store Topological
+        # Sort starting from all vertices one by one
+        for i in range(self.V):
+            if visited[i] == False:
+                self.topologicalSortUtil(i, visited, stack)
+
+        # Print contents of the stack
+        return stack[0]
+
+
+testCase = int(input()) + 1
+for i in range(1, testCase):
+    V = int(input())
+    g = Graph(V)
+    for j in range(V):
+        u, v = map(int, input().split())
+        g.addEdge(u-1, v-1)
+    print("Case {0}: {1}".format(i, g.topologicalSort()+1))
+
+
