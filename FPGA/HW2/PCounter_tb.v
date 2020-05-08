7762bbcd3ff02a9147fcaaf984065b84ffea20f9
Maryam Saeedmehr <maryamsaeedmehr@ec.iut.ac.ir>
2020-05-08 16:37:54 +0430

    Update PCounter_tb.v

--- FPGA/HW2/PCounter_tb.v
+++ FPGA/HW2/PCounter_tb.v
@@ -1,22 +1,29 @@
+`timescale 1ns / 1ps
+
 module PCounter_tb();
 
+	reg enable=0;
 	reg clk=0;
 	reg reset=0;
+	integer i;
 	wire [3:0] cnt_out;
 	wire out;
 
-	PCounter #(.MAX_SIZE(4),.p(10)) U1 (.clk(clk),.cnt_out(cnt_out),.out(out),.reset(reset));
+	always #5 clk = ~clk;
+
+	PCounter #(.MAX_SIZE(4),.p(10)) U1 (.enable(enable),.clk(clk),.cnt_out(cnt_out),.out(out),.reset(reset));
 
 	initial 
 	begin 
-		$monitor("RESER=%s Parameter=10 cnt_out=%d" ,reset?"OFF":"ON",cnt_out);
-
-		     reset = 1 ;
-		#70  reset = 0 ;
-		#50  reset = 1 ;
-		#100 reset = 0 ;
+	
+		for(i = 0 ; i < 4 ; i = i + 1 )
+		begin 
+			{enable,reset} = i;
+			#50;
+			$monitor("ENABLE=%s , RESER=%s Parameter=10 cnt_out=%d" ,enable?"ON":"OFF",reset?"OFF":"ON",cnt_out);
+		end
+		#100;
+		$monitor("----------------Simulation ENDs----------------");
 	end
 		
-	always #5 clk=~clk;
-
 endmodule
