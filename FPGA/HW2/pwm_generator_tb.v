84ba8e02cdfb0b3482e6b7f465992203c32e49f0
Maryam Saeedmehr <maryamsaeedmehr@ec.iut.ac.ir>
2020-05-08 16:40:33 +0430

    Update pwm_generator_tb.v

--- FPGA/HW2/pwm_generator_tb.v
+++ FPGA/HW2/pwm_generator_tb.v
@@ -1,17 +1,39 @@
-module pwm_generator_tb();
-	
+`timescale 1ns / 1ps
+
+module pwm_generator_tb;
+
+	// Inputs
 	reg clk=0;
 	reg high_wr=0;
 	reg low_wr=0;
-	reg [15:0] data_in = 0;
+	reg [15:0] data_in=0;
+	reg [6:0] DutyCycle=7'bz;
+
+	// Outputs
 	wire pwm_out;
-	reg [6:0] DutyCycle=0;
+
+	// Instantiate the Unit Under Test (UUT)
+	pwm_generator uut (
+		.clk(clk), 
+		.high_wr(high_wr), 
+		.low_wr(low_wr), 
+		.data_in(data_in), 
+		.pwm_out(pwm_out)
+	);
 
 	always #5 clk=~clk;
 
-	pwm_generator U1 (clk,high_wr,low_wr,data_in,pwm_out);
-	
 	initial begin
+		// Initialize Inputs
+		clk = 0;
+		high_wr = 0;
+		low_wr = 0;
+		data_in = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
 		$monitor("High_write = %s , Low_write = %s , Data_in = %d ==>> Duty Cycle = %d percent",high_wr?"ON":"OFF",low_wr?"ON":"OFF",data_in,DutyCycle);
 		
 		#30	DutyCycle = 10;
@@ -37,8 +59,7 @@ module pwm_generator_tb();
 		#5	data_in = 1;
 			low_wr = 1;
 		#10	low_wr = 0;
-
-
-	end	
-	
+		
+	end
+      
 endmodule
