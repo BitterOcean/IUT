47c4742b7a12871b3c187e42f48acfe85385cd26
Maryam Saeedmehr <maryamsaeedmehr@ec.iut.ac.ir>
2020-05-08 16:38:44 +0430

    Update Stopwatch_tb.v

--- FPGA/HW2/Stopwatch_tb.v
+++ FPGA/HW2/Stopwatch_tb.v
@@ -1,26 +1,51 @@
-`timescale 1 us/100 ps // clk = 1 MHz
+`timescale 1us / 1ps
 
-module Stopwatch_tb();
+module Stopwatch_tb;
 
-	wire [5:0] second,minute;
-	wire [4:0] hour,day;
-	wire [3:0] month;
-	wire [6:0] year;
+	// Inputs
 	reg clk=0;
 	reg reset=1;
 	reg start=0;
 	reg stop=0;
+	integer i;
 
-	Stopwatch U1 (clk,reset,start,stop,second,minute,hour,day,month,year);
+	// Outputs
+	wire [5:0] second;
+	wire [5:0] minute;
+	wire [4:0] hour;
+	wire [4:0] day;
+	wire [3:0] month;
+	wire [6:0] year;
 
-	initial begin
-		$monitor(" START : %s - RESET : %s - STOP : %s *** Stopwatch : %d/%d/%d - %d:%d:%d",start?"ON":"OFF",reset?"OFF":"ON",stop?"ON":"OFF",year,month,day,hour,minute,second);
-		#5  start=1; 
-		#10 reset=0;
-		#3  reset=1;
-		#20 stop=1;
-	end
-		
+	// Instantiate the Unit Under Test (UUT)
+	Stopwatch uut (
+		.start(start),
+		.stop(stop),
+		.clk(clk), 
+		.reset(reset), 
+		.second(second), 
+		.minute(minute), 
+		.hour(hour), 
+		.day(day), 
+		.month(month), 
+		.year(year)
+	);
+
+
 	always #0.5 clk=~clk;
 
+	initial begin
+
+		for(i = 0 ; i < 4 ; i = i + 1 )
+		begin 
+			{start,reset} = i;
+			#50;
+			$monitor("START : %s - RESET : %s - STOP : %s *** Stopwatch : %d/%d/%d - %d:%d:%d",start?"ON":"OFF",reset?"OFF":"ON",stop?"ON":"OFF",year,month,day,hour,minute,second);
+		end
+		#500;
+		stop = 1;
+		$monitor("---------------------Simulation ENDs---------------------");
+
+	end
+      
 endmodule
