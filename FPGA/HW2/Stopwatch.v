b7d6e842cb643132b2aa0b87aa53572baf171863
Maryam Saeedmehr <maryamsaeedmehr@ec.iut.ac.ir>
2020-05-08 16:38:22 +0430

    Update Stopwatch.v

--- FPGA/HW2/Stopwatch.v
+++ FPGA/HW2/Stopwatch.v
@@ -1,7 +1,40 @@
-module Stopwatch(input clk, reset, start, stop, output [5:0] second,minute, output [4:0] hour,day, output[3:0] month, output [6:0] year);
+`timescale 1ns / 1ps
 
-wire Syncked_clk;
-and U0 (Syncked_clk , clk, start, ~stop);
-DayCounter U1 (Syncked_clk,reset,second,minute,hour,day,month,year);
+module Stopwatch(
+	input clk, 
+	input reset,
+	input start, 
+	input stop, 
+	output [5:0] second,
+	output [5:0] minute, 
+	output [4:0] hour,
+	output [4:0] day, 
+	output[3:0] month, 
+	output [6:0] year
+	);
+
+	reg enable=0;
+
+	DayCounter uu1(
+		.enable(enable),
+		.clk(clk),
+		.reset(reset),
+		.second(second),
+		.minute(minute),
+		.hour(hour),
+		.day(day),
+		.month(month),
+		.year(year)
+	);
+	
+	always @(posedge start or posedge stop)
+	begin
+	
+		if(start==1)
+			enable <= 1;
+		if(stop==1)
+			enable <= 0; // stop has a higher priority
+	
+	end
 
 endmodule
