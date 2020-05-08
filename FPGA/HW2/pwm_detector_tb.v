a6e480559c7f1325a25c642c9a86028af90add8c
Maryam Saeedmehr <maryamsaeedmehr@ec.iut.ac.ir>
2020-05-08 16:39:34 +0430

    Update pwm_detector_tb.v

--- FPGA/HW2/pwm_detector_tb.v
+++ FPGA/HW2/pwm_detector_tb.v
@@ -1,10 +1,24 @@
-module pwm_detector_tb();
-	
+`timescale 1ns / 1ps
+
+module pwm_detector_tb;
+
+	// Inputs
 	reg clk=0;
-	reg high_r=0;
-	reg low_r=0;
 	reg pwm_in=0;
+	reg high_read=0;
+	reg low_read=0;
+
+	// Outputs
 	wire [15:0] data_out;
+
+	// Instantiate the Unit Under Test (UUT)
+	pwm_detector uut (
+		.clk(clk), 
+		.pwm_in(pwm_in), 
+		.high_read(high_read), 
+		.low_read(low_read), 
+		.data_out(data_out)
+	);
 
 	always #5 clk=~clk;
 
@@ -15,9 +29,17 @@ module pwm_detector_tb();
 		repeat (3) begin #30 pwm_in = 1; #30 pwm_in = 0; end
 	end
 
-	pwm_detector U1 (clk,pwm_in,high_r,low_r,data_out);
-	
 	initial begin
+		// Initialize Inputs
+		clk = 0;
+		pwm_in = 0;
+		high_read = 0;
+		low_read = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
 		$monitor("%d : Data_Out = %d ==> %s ",$time ,data_out,high_r?"High Value":(low_r?"Low Value":"Not Set"));
 		
 		#30	high_r = 1;
@@ -36,7 +58,7 @@ module pwm_detector_tb();
 		#30	low_r = 0;
 			high_r = 1;
 		#30	high_r = 0;
-
-	end	
-	
+		
+	end
+      
 endmodule
